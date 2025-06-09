968f9a6bc28dc129f71f092c8725e674b118b03b
Rizq0 <joe_ethan2@hotmail.com>
2025-06-09 23:55:01 +0100

    add: countdown message functionality added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using ArcRaidersCountdown.Services;
+class Program
+{     static void Main(string[] args)
+    {
+        DateTime releaseDate = new DateTime(2025, 10, 30);
+        DateTime today = DateTime.Today;
+        string countdownMessage = CountdownService.GetCountdownMessage(today, releaseDate);
+        Console.WriteLine(countdownMessage);
+    }
+}
