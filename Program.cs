f1e588ded0bc8e2c1ed27dd8e8cd2cdbce927e8a
Rizq0 <joe_ethan2@hotmail.com>
2025-06-16 18:57:08 +0100

    add: webservice added, testing GET only

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using ArcRaidersCountdown.Services;
+using System.Net.Http;
 class Program
-{     static void Main(string[] args)
+{     static async Task Main(string[] args)
     {
         DateTime releaseDate = new DateTime(2025, 10, 30);
         DateTime today = DateTime.Today;
         string countdownMessage = CountdownService.GetCountdownMessage(today, releaseDate);
+        var service = new WebService(new HttpClient());
+        string testPing = await service.SendDiscordMessage("https://jsonplaceholder.typicode.com/posts/1", countdownMessage);
         Console.WriteLine(countdownMessage);
+        Console.WriteLine(testPing);
     }
 }
+
+
---
+++ Services/WebService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace ArcRaidersCountdown.Services
+{
+    internal class WebService
+    {
+        private readonly HttpClient _httpClient;
+
+        public WebService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> SendDiscordMessage(string webhookUrl, string message)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync(webhookUrl);
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                string errorMessage = $"Request error: {e.Message}";
+                Console.WriteLine(errorMessage);
+                return errorMessage;
+            }
+
+        }
+    }
+}
