6743335953c066062a929928516cb84789e3ed83
Rizq0 <joe_ethan2@hotmail.com>
2025-06-29 00:59:35 +0100

    add/remove: countdown logic moved into arc raiders service

--- Services/ArcRaidersService.cs
+++ Services/ArcRaidersService.cs
@@ -7,14 +7,32 @@ using System.Net.Http;
 
 namespace ArcRaidersCountdown.Services
 {
-    internal class WebService
+    internal class ArcRaidersService
     {
         private readonly HttpClient _httpClient;
 
-        public WebService(HttpClient httpClient)
+        public ArcRaidersService(HttpClient httpClient)
         {
             _httpClient = httpClient;
         }
+        
+        public static string GetCountdownMessage(DateTime today, DateTime releaseDate)
+        {
+            int daysUntilRelease = (releaseDate - today).Days;
+
+            if (daysUntilRelease > 0)
+            {
+                return $"ARC Raiders releases in {daysUntilRelease} days!";
+            }
+            else if (daysUntilRelease == 0)
+            {
+                return "ARC Raiders releases today!";
+            }
+            else
+            {
+                return "ARC Raiders has already been released.";
+            }
+        } 
 
         public async Task SendDiscordMessage(string webhookUrl, string message)
         {
