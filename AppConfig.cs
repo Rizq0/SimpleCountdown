3b139b1cf839b4e12d80cd8d9d803ef2e4c0d65e
Rizq0 <joe_ethan2@hotmail.com>
2025-06-29 00:59:10 +0100

    add: AppConfig file to handle env variables logic

---
+++ AppConfig.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.Extensions.Configuration;
+namespace ArcRaidersCountdown;
+
+public class AppConfig
+{
+    public string WebhookUrl { get; }
+    public string RequiredService { get; }
+    public DateTime TargetDate { get; }
+
+    public AppConfig()
+    {
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .AddEnvironmentVariables()    
+            .Build();
+        
+        WebhookUrl = config["DISCORD_WEBHOOK"] ?? throw new Exception("DISCORD_WEBHOOK environment variable is not set.");
+        RequiredService = config["SERVICE"] ?? throw new Exception("SERVICE environment variable is not set.");
+        var targetDateString = config["TARGET"] ?? throw new Exception("TARGET environment variable is not set.");
+
+        if (!DateTime.TryParse(targetDateString, out var targetDate))
+            throw new Exception("Invalid TARGET date format. Use yyyy-MM-dd.");
+        TargetDate = targetDate;
+    }
+}   
